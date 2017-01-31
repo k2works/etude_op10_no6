cc2d2d721313b4b22682316ea932293c08fafc53
k2works <kakimomokuri@gmail.com>
2017-01-31 11:21:53 +0900

    Step11

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,16 @@ require "spec_helper"
 include EtudeOp10No6
 
 describe Game do
-  it "should scored when one throw" do
-    f = Game.new
-    f.add(5)
-    expect(f.score).to eq(5)
+  it 'should scored when one throw' do
+    g = Game.new
+    g.add(5)
+    expect(g.score).to eq(5)
+  end
+
+  it 'should scored when two throw' do
+    g = Game.new
+    g.add(5)
+    g.add(4)
+    expect(g.score).to eq(9)
   end
 end

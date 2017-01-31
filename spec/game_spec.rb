77e4d2fbe3a2a68890b995fb2778fca43b778923
k2works <kakimomokuri@gmail.com>
2017-01-31 11:28:21 +0900

    Step12

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,10 +8,21 @@ describe Game do
     expect(g.score).to eq(5)
   end
 
-  it 'should scored when two throw' do
+  it 'should scored when two throws' do
     g = Game.new
     g.add(5)
     g.add(4)
     expect(g.score).to eq(9)
+  end
+
+  it 'should scored when four throws' do
+    g = Game.new
+    g.add(5)
+    g.add(4)
+    g.add(7)
+    g.add(2)
+    expect(g.score).to eq(18)
+    expect(g.score_for_frame(1)).to eq(1)
+    expect(g.score_for_frame(2)).to eq(18)
   end
 end

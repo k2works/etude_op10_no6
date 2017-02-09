be7c9a71f9f44fe76f08f7dd9f486b55848bb2c7
k2works <kakimomokuri@gmail.com>
2017-02-09 10:19:37 +0900

    Step15

--- lib/etude_op10_no6/game.rb
+++ lib/etude_op10_no6/game.rb
@@ -1,15 +1,31 @@
 module EtudeOp10No6
   class Game
     def initialize
-      @score = 0
+      @its_score = 0
+      @its_throws = Array.new(21,0)
+      @its_current_throw = 0
     end
 
     def score
-      @score
+      @its_score
     end
 
     def add(pins)
-      @score += pins
+      @its_throws[@its_current_throw] = pins
+      @its_current_throw+=1
+      @its_score+=pins
+    end
+
+    def score_for_frame(the_frame)
+      score = 0
+      ball = 0
+      current_frame = 0
+      while current_frame < the_frame
+        score += @its_throws[ball] + @its_throws[ball+1]
+        ball+=2
+        current_frame+=1
+      end
+      score
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,33 @@
 require "spec_helper"
 
-describe "when one throw" do
-  it "should be scored" do
-    f = Game.new
-    f.add(5)
-    expect(f.score).to eq(5)
+describe "bowling game" do
+  context "when one throw" do
+    it "should be scored" do
+      g = Game.new
+      g.add(5)
+      expect(g.score).to eq(5)
+    end
+  end
+
+  context "when two throws" do
+    it "should be scored" do
+      g = Game.new
+      g.add(5)
+      g.add(4)
+      expect(g.score).to eq(9)
+    end
+  end
+
+  context "when four throws" do
+    it "should be scred" do
+      g = Game.new
+      g.add(5)
+      g.add(4)
+      g.add(7)
+      g.add(2)
+      expect(g.score).to eq(18)
+      expect(g.score_for_frame(1)).to eq(9)
+      expect(g.score_for_frame(2)).to eq(18)
+    end
   end
 end

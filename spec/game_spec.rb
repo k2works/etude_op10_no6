e3507a3b3dc6076f8087463eec8ebe884b0343e6
k2works <kakimomokuri@gmail.com>
2017-02-11 12:48:38 +0900

    Step23

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -27,16 +27,6 @@ describe ".score" do
       expect(g.score).to eq(18)
     end
   end
-
-  context "when after spare" do
-    it "is scored" do
-      g.add(3)
-      g.add(7)
-      g.add(3)
-      g.add(2)
-      expect(g.score).to eq(18)
-    end
-  end
 end
 
 describe ".score_for_frame" do
@@ -69,6 +59,7 @@ describe ".score_for_frame" do
       g.add(3)
       g.add(2)
       expect(g.score_for_frame(1)).to eq(13)
+      expect(g.score_for_frame(2)).to eq(18)
     end
   end
 end

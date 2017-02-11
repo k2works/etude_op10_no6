857c257fd15f562b5887116f0915b854387a3f39
k2works <kakimomokuri@gmail.com>
2017-02-11 12:38:37 +0900

    Edit spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,23 +1,39 @@
 require "spec_helper"
 
-describe "bowling game" do
+describe ".score" do
   let(:g) { Game.new }
 
   context "when one throw" do
-    it "should be scored" do
+    it "is scored" do
       g.add(5)
       expect(g.score).to eq(5)
     end
   end
 
   context "when two throws" do
-    it "should be scored" do
+    it "is scored" do
       g.add(5)
       g.add(4)
       expect(g.score).to eq(9)
     end
+  end
 
-    it "should be scored spare" do
+  context "when four throws" do
+    it "is scored" do
+      g.add(5)
+      g.add(4)
+      g.add(7)
+      g.add(2)
+      expect(g.score).to eq(18)
+    end
+  end
+end
+
+describe ".score_for_frame" do
+  let(:g) { Game.new }
+
+  context "when spare" do
+    it "is scored by frame" do
       g.add(3)
       g.add(7)
       g.add(3)
@@ -26,12 +42,11 @@ describe "bowling game" do
   end
 
   context "when four throws" do
-    it "should be scred" do
+    it "is scored by farme" do
       g.add(5)
       g.add(4)
       g.add(7)
       g.add(2)
-      expect(g.score).to eq(18)
       expect(g.score_for_frame(1)).to eq(9)
       expect(g.score_for_frame(2)).to eq(18)
     end
